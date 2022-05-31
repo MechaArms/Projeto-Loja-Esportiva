1198cd5c0fe17184e4289cc639cfb91ad9ed1fb1
Romero <103903074+MechaArms@users.noreply.github.com>
2022-05-31 09:21:45 -0300

    Update lojaEsportiva.py
    - Adição de cancelamento de itens e cupons
    - Criação e organização de funções

--- lojaEsportiva.py
+++ lojaEsportiva.py
@@ -38,7 +38,7 @@ def getDados():
         else:
             lista.append(produto) #codigo produto
             indice = codigoProdutos.index(produto)
-            quantidade = int(input("Quantidade: "))
+            quantidade = int(input('Quantidade: '))
             if quantidade <= 0:
                 print('[Numero Inválido]')
                 print(f'O produto {cont} foi cancelado!')
@@ -52,10 +52,10 @@ def getDados():
                 prod.append(listaProdutos[indice]) #descrição produto
         print('')
         cont += 1
-    SetDados(lista,prod,qt,und,pedido)
+    setDados(lista,prod,qt,und,pedido)
 
 
-def SetDados(lista,prod,qt,und,pedido): #impressão tela
+def setDados(lista,prod,qt,und,pedido): #impressão tela
     print('\n' * 2)
     print('{:^50}'.format('CAIXA LIVRE - VENDA'))
     print('{:<5}{:>7}{:>15}{:>6}{:>10}{:>13}'.format('Item','Codigo','Nome','Qt','Val Unit','Val Item'))
@@ -69,17 +69,32 @@ def SetDados(lista,prod,qt,und,pedido): #impressão tela
     print('{:>43}{:>13.2f}'.format('Total R$',total))
     total_fecha_caixa.append(total) #adicionando o valor total da nota para fechar o caixa
     print('\n'*2)
-    #troco
-    troco = pagamento(total)
-    print(f'\nO valor do troco é de: R$ {troco:.2f}')
-    
-    #limpeza da lista de compra
-    lista.clear()
-    prod.clear()
-    qt.clear()
-    und.clear()
-    pedido.clear()
-    checkNovoCupom() 
+
+    #confirma compra
+    confirmaLista(total) 
+        
+
+def confirmaLista(total):
+    confirmarListaCompra = int(input('\nDeseja finalizar a lista ou cancelar um item da lista? \n [finalizar = 0, cancelar item = 1, cancelar nota = 9]: '))
+    if confirmarListaCompra == 0:
+        print('\nLista de compras concluida!\n')
+        #troco
+        troco = pagamento(total)
+        print(f'\nO valor do troco é de: R$ {troco:.2f}')
+        #limpeza da lista de compra e abertura de uma nova
+        deleteCupom()
+        checkNovoCupom() 
+    elif confirmarListaCompra == 1:
+        cancelamentoItem()
+    elif confirmarListaCompra == 9:
+        print('O cupom nº ',len(n_cupom),' foi cancelado!')
+        total_fecha_caixa[-1] = 0 #zera a nota atual
+        deleteCupom()
+        checkNovoCupom()
+    else:
+        print('[Numero Inválido]')
+        confirmaLista()
+
 
 def pagamento(total):
     valorPago = float(input('Digite o valor a pagar: R$ '))
@@ -87,8 +102,9 @@ def pagamento(total):
     troco = valorPago - total
     return troco
 
+
 def checkNovoCupom(): #pergunta novo cupom ou fechamento
-    perg = int(input('\nDeseja fechar o caixa? \n[sim=1 , não=2]: '))
+    perg = int(input('\nDeseja fechar o caixa? \n[sim = 1 , não = 2]: '))
     if perg == 1:
         fechamento(total_fecha_caixa)
     elif perg == 2:
@@ -104,13 +120,38 @@ def checkNovoCupom(): #pergunta novo cupom ou fechamento
             else:
                 continue
         
+def deleteCupom():
+    lista.clear()
+    prod.clear()
+    qt.clear()
+    und.clear()
+    pedido.clear()
+
+
+def cancelamentoItem():
+    checkLista = int(input('Digite o numero do item na lista que deseja excluir: '))
+    checkLista -= 1
+    if checkLista > len(lista) or checkLista < 0:
+        print('[Numero Inválido]')
+        cancelamentoItem()
+    else:
+        lista.pop(checkLista)
+        prod.pop(checkLista)
+        qt.pop(checkLista)
+        und.pop(checkLista)
+        pedido.pop(checkLista)
+    setDados(lista,prod,qt,und,pedido)
+
 
 def fechamento(total_fecha_caixa): #fechamento caixa
     print('\n' * 2)
     print('--Fechamento de Caixa--')
     print('\nQuantidade de cupons emitidos: ', len(total_fecha_caixa))
     for i in range(len(total_fecha_caixa)):
-        print(f'Cupom nº {i+1} R$ {total_fecha_caixa[i]:.2f}')
+        if total_fecha_caixa[i] == 0:
+            print(f'Cupom nº {i+1} foi cancelado')
+        else:
+            print(f'Cupom nº {i+1} R$ {total_fecha_caixa[i]:.2f}')
     print('Total R$ {:<10.2f}'.format(sum(total_fecha_caixa)))
 
 getDados() #executando o codigo
